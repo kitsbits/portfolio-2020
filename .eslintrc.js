1db98c6d9d841e6ef3ea8b53c6b00687cdd267e6
Kit Masaracchia <kit.masaracchia@gmail.com>
2020-08-09 10:46:03 -0600

    Fix linting setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const prettierOptions = JSON.parse(fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'));
 
 module.exports = {
